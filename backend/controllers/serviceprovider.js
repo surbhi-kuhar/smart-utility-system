987bf9dfae2c967d54e9430fd9aa5a87f12a2020
surbhikuhar <surbhikuhar07@gmail.com>
2025-01-15 16:11:02 +0530

    changes for deployment

--- backend/controllers/serviceprovider.js
+++ backend/controllers/serviceprovider.js
@@ -1,6 +1,6 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 module.exports.createProvider = async (req, res, next) => {
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,7 +1,7 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const Cookies = require("cookies");
 
 module.exports.createUser = async (req, res, next) => {
